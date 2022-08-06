25c255db2a5da0a89a3c2aabfc62b7832731b31a
KSIDDHARDHA <siddhardhasiddhu46@gmail.com>
2022-08-06 17:11:17 +0530

    work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Work from './work';
+//import Contact from './contact';
+function App() {
+  return (
+    <div>
+ 
+    <Work/>
+    </div>
+  );
+}
+
+export default App;
